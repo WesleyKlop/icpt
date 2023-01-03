0ce16bc09cb5e3bdaeca3c684e96f93032fa6c6a
Wesley Klop <s1106689@student.hsleiden.nl>
2023-01-03 21:35:50 +0100

    Implement test runner process isolation

--- javascript/packages/test-framework/bin/test-framework.js
+++ javascript/packages/test-framework/bin/test-framework.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
-import { argv } from 'process'
-import path from 'path'
+import process from 'node:process'
+import cluster from 'node:cluster'
+import path from 'node:path'
 import { isMain } from '@icpt/shared'
 import { startTestSuite } from '../src/index.js'
+import { createTestContext } from '../src/context.js'
+import { executeTestsInContext } from '../src/runner.js'
 
-if ((await isMain(import.meta.url)) === true) {
-  const whereToDiscoverTests = argv[2]
+if ((await isMain(import.meta.url)) === true && cluster.isPrimary) {
+  const whereToDiscoverTests = process.argv[2]
   await startTestSuite(path.resolve('.', whereToDiscoverTests))
+}
+
+if (cluster.isWorker) {
+  process.send({ type: 'ready' })
+  process.on('message', async (message) => {
+    if (message.type === 'exec-test') {
+      const context = await createTestContext(message.file)
+      const results = await executeTestsInContext(context)
+      process.send({ type: 'test-results', results })
+      process.exit(0)
+    }
+  })
 }
--- javascript/packages/test-framework/src/index.js
+++ javascript/packages/test-framework/src/index.js
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import cluster from 'node:cluster'
 import { createTestContext } from './context.js'
 import { discoverTestFiles } from './discovery.js'
 import { executeTestsInContext } from './runner.js'
@@ -18,13 +19,24 @@ const printTestResults = (resultsByFile) => {
  * @param {string} folder where to find test files
  */
 export const startTestSuite = async (folder) => {
+  const waitGroup = []
   const fileResults = new Map()
   for await (const file of discoverTestFiles(folder)) {
-    const context = await createTestContext(file)
-    const results = await executeTestsInContext(context)
-
-    fileResults.set(path.relative('.', file), results)
+    const worker = cluster.fork()
+    worker
+      .on('message', (message) => {
+        if (message.type === 'test-results') {
+          fileResults.set(path.relative('.', file), message.results)
+        } else if (message.type === 'ready') {
+          worker.send({ type: 'exec-test', file })
+        }
+      })
+      .on('error', (...args) => {
+        console.log(args)
+      })
+    waitGroup.push(new Promise((res) => worker.on('exit', () => res())))
   }
 
+  await Promise.all(waitGroup)
   printTestResults(fileResults)
 }
